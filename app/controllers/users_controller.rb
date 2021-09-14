307e7e5ad85c6e7de6638c4b72e8e94b41d3b75f
Corinne <corinne.boesch@gmail.com>
2021-09-14 17:30:02 +0100

    working on create user in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,23 @@
 class UsersController < ApplicationController
   def login; end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @post = User.create(post_params)
+    if @post.save
+      redirect_to #to login?
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:user).permit(:username, :password,
+       :first_name, :last_name, :email)
+  end
 end
