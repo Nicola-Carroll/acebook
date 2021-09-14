00a08ac9a92dacca8f68f120d110fd7e26e0d4b8
Yousuf Mohamed <yousuf_mohamed@hotmail.com>
2021-09-14 17:29:13 +0100

    WIP adding comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,11 @@
 class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new(post_id: params[:post_id])
+  end
+
+  def create
+    @post = Post.find(params[:id])
+    @comment = @post.comments.create(params[:message])
+    redirect_to post_path(@post)
+  end
 end
