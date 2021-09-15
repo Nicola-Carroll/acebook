4c6f326a9446e2d2a8697a8bf9a749f6640f84b4
Yousuf Mohamed <yousuf_mohamed@hotmail.com>
2021-09-15 16:59:15 +0100

    WIP comments controller tests

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "Comments", type: :controller do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe CommentsController, type: :controller do
+  describe "POST /" do
+    it "responds with 200" do
+      post :create, params: { comment: { message: "Hello, world!", user_id: 1}, post_id: 1 }
+      @post = Post.find(1)
+      expect(response).to redirect_to(post_path(@post)) # This test is passing but needs to be changed
+    end
+
+    it "creates a comment" do
+      post :create, params: { comment: { message: "Hello, world!", user_id: 1}, post_id: 1 }
+      expect(Comment.find_by(message: "Hello, world!")).to be
+    end
   end
 end
