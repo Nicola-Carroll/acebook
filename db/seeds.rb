9faeb11074eb8570b2ff6d91bf4639115fd2faeb
Suhani Zaman <suhanizaman15@gmail.com>
2021-09-15 19:26:52 +0400

    started with TDD

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,16 +27,22 @@ users = [
     last_name: 'Attenborough'
   }]
 
-  posts = [
-    {
-      message: "Hello Graeme",
-      user_id: 1
-    },
-    {
-      message: "Hello Apps",
-      user_id: 2
-    }
-  ]
-
 users.each { |user| User.create(user) }
+
+posts = [
+    
+  { user_id: 1,
+    message: "First post",
+    likes: 1
+  },
+  { user_id: 2,
+    message: "Second post",
+    likes: 2
+  },
+  { user_id: 3,
+    message: "Third post",
+    likes: 3
+  }]
+
 posts.each { |post| Post.create(post) }
+ 
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Comments", type: :request do
+RSpec.describe "Comments", type: :controller do
   describe "GET /index" do
     pending "add some examples (or delete) #{__FILE__}"
   end
