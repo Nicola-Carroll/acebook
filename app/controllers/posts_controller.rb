12340f3eeaaded9bb410ee5df261dd80ac2b7e7d
Fiona Apps <wikipanyd@gmail.com>
2021-09-16 16:49:37 +0100

    We have posts now in descending order of creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,33 +3,20 @@ class PostsController < ApplicationController
   def index
     # this method is going to need parameters passed to it
     @new_post = Post.new
-    @list_posts = Post.all
+    @user_id = session[:user_id]
+    @list_posts = Post.all.order(created_at: :desc)
   end
 
   def create
     @new_post = Post.create(post_params)
-    # user id session[:user_id]
-    # if post_params[:message] == ""
     redirect_to '/posts'
   end
-
   
   private
 
   def post_params
-    params.require(:post).permit(:message)
+    params.require(:post).permit(:message, :user_id)
   end
 
 
 end
-
-# First commit
-
-
-  # def new
-  #   @new_post = Post.new
-  # end
-#
-
-
-
