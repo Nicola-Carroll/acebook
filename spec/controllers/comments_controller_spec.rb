168513413101cea123c683350be5482c2f4f4f39
Yousuf Mohamed <yousuf_mohamed@hotmail.com>
2021-09-17 15:08:20 +0100

    refactored comments controller test

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -4,8 +4,7 @@ RSpec.describe CommentsController, type: :controller do
   describe "POST /" do
     it "responds with 200" do
       post :create, params: { comment: { message: "Hello, world!", user_id: 1}, post_id: 1 }
-      @post = Post.find(1)
-      expect(response).to redirect_to(post_path(@post)) # This test is passing but needs to be changed
+      expect(response).to redirect_to(post_path(Post.find(1)))
     end
 
     it "creates a comment" do
