e3d2b96360c80bcd15c992956c68cf72a860b50e
Yousuf Mohamed <yousuf_mohamed@hotmail.com>
2021-09-17 13:52:07 +0100

    fixed feature test, refactored link_to add likes to comments

--- spec/features/user_can_make_comments_spec.rb
+++ spec/features/user_can_make_comments_spec.rb
@@ -2,13 +2,12 @@ require 'rails_helper'
 
 RSpec.feature "Comments", type: :feature do
     scenario "Can submit posts and view them" do
-      visit "/login"
-      fill_in "email"
-      fill_in "password"
+      visit "http://localhost:3000/login" # need to change default domain in capybara/rspec
+      fill_in "email", with: "joebloggs@example.com"
+      fill_in "password", with: "dQlRIF"
       click_button "Login"
-      visit "/posts"
       first("a").click
-      fill_in "Message", with: "Hello, world!"
+      fill_in "comment_message", with: "Hello, world!"
       click_button "Submit"
       expect(page).to have_content("Hello, world!")
     end
