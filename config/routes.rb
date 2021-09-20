6500f112b2aacf1cb1d6ab0c0355f5500302217c
Nicola-Carroll <nicolaccarroll@gmail.com>
2021-09-20 15:26:41 +0100

    heroku routes broken

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,12 @@ Rails.application.routes.draw do
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
+
+  get 'logout', to: 'sessions#destroy'
+  delete 'logout', to: 'sessions#destroy'
+
   get 'signup', to: 'users#new'
   post 'signup', to: 'users#create'
 
   root to: 'users#new'
-
-  get '/', to: 'users#new'
 end
