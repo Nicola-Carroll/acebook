6e37c8c8a804b83d6af8a129680479e7753d61e5
Suhani Zaman <suhanizaman15@gmail.com>
2021-09-20 19:20:42 +0400

    created narbar with login/signout

--- config/routes.rb
+++ config/routes.rb
@@ -19,14 +19,3 @@ Rails
     root to: 'users#new'
   end
 
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts do
-    resources :comments
-  end
-
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  get 'signup', to: 'users#new'
-  post 'signup', to: 'users#create'
-end
