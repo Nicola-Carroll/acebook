d348adc0f66b79100960defd9cf2a37362833227
Nicola-Carroll <nicolaccarroll@gmail.com>
2021-09-20 16:39:27 +0100

    flash notifications etc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.save
-<<<<<<< HEAD
-      redirect_to login_path, notice: 'User created :D'
-=======
       redirect_to login_path, notice: 'Signed up successfully! Please login'
->>>>>>> 2126264909c5afe4beab4459fa2b18187d291f09
     else
       render 'new'
     end
