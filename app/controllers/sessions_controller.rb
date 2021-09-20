565ae45b0fcb0721074f2381a20c44110a70433e
Nicola-Carroll <nicolaccarroll@gmail.com>
2021-09-20 12:17:54 +0100

    fixing flash message on login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,23 @@
 class SessionsController < ApplicationController
   skip_before_action :authorised
 
-  def new; end
+  def new
+    p "params when new is called #{params}"
+  end
 
   def create
+    p "params when create is called #{params}"
     @user = User.find_by(email: params[:email])
 
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to posts_url
     else
+      p "user login not valid so redirect to login path"
       flash[:alert] = "Invalid email or password"   
       redirect_to login_path
     end
   end
-
-  def login; end
 
   def destroy
     flash.clear
