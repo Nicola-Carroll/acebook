cf190127cd6b254d39cdf7bdce7509aa595f2629
Yousuf Mohamed <yousuf_mohamed@hotmail.com>
2021-09-21 15:49:24 +0100

    Can upload profile images and display them

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
   helper_method :logged_in?
+  helper_method :user_avatar
 
   def current_user
     User.find_by(id: session[:user_id])
@@ -9,5 +10,14 @@ class ApplicationController < ActionController::Base
 
   def logged_in?
     !current_user.nil?
+  end
+
+  def user_avatar(user_id)
+    user = User.find(user_id)
+    if user.profile_image.attached?
+      user.profile_image
+    else
+      'default_avatar.jpg'
+    end
   end
 end
