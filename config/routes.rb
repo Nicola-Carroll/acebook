befc5dbbae12afcb51b736de221a7d5b9337f56f
Nicola-Carroll <nicolaccarroll@gmail.com>
2021-09-21 12:16:30 +0100

    fixing merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts do
@@ -16,28 +15,3 @@ Rails.application.routes.draw do
 
   root to: 'users#new'
 end
-=======
-
-Rails
-  .application
-  .routes
-  .draw do
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-    resources :posts do
-      resources :comments
-    end
-
-    get 'login', to: 'sessions#new'
-    post 'login', to: 'sessions#create'
-
-    get 'logout', to: 'sessions#destroy'
-    delete 'logout', to: 'sessions#destroy'
-
-    get 'signup', to: 'users#new'
-    post 'signup', to: 'users#create'
-
-    root to: 'users#new'
-  end
-
->>>>>>> 16abc46a082338d420cac3699e1cb1315ebe9c80
