ae467e4fe79b3ee92ee964af46c80da73da652c5
Yousuf Mohamed <yousuf_mohamed@hotmail.com>
2021-09-21 16:57:02 +0100

    User avatar validation complete, post image validation WIP

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   has_one_attached :post_image
   has_many :comments
-  validates :profile_image, content_type: ['image/png', 'image/jpg', 'image/jpeg'],
-            size: { less_than: 1.megabytes , message: 'Uploaded image size is too big' }
+  validates :post_image, content_type: ['image/png', 'image/jpg', 'image/jpeg'],
+            size: { less_than: 5.megabytes , message: 'Uploaded image size is too big' }
 end
