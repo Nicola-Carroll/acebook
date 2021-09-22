72d7033c5245f943d4d7be079215cfb2703c3754
Nicola-Carroll <nicolaccarroll@gmail.com>
2021-09-22 13:19:33 +0100

    added react

--- app/javascript/packs/postLikes.js
+++ app/javascript/packs/postLikes.js
@@ -1,12 +1,36 @@
-function updateLikes() {
-  let greeting = "Hello, " + name + "!";
-  console.log(greeting);
-  alert(greeting);
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.getElementById("1").addEventListener("click", () => {
+  console.log("hello!");
+  console.log(this.id);
+  console.log(document.getElementById("1").getAttribute("data"));
+  alert("hello!");
+});
+// document.
+//   const clickButton = document.getElementById("1");
+//   clickButton.addEventListener('click', (event) => {
+//       hello('JavaScript')
+//   });
+// });
+
+class Posts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.posts;
+  }
+
+  render() {
+    return <h1>hello</h1>;
+  }
 }
 
-document.addEventListener('click', () => {
-  const clickButton = document.getElementById("greet-user-button");
-  clickButton.addEventListener('click', (event) => {
-      hello('JavaScript')
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  const node = document.getElementById("posts_data");
+  const data = JSON.parse(node.getAttribute("data"));
+  console.log(data);
+  ReactDOM.render(
+    <Posts posts={data} />,
+    document.body.appendChild(document.createElement("div"))
+  );
 });
