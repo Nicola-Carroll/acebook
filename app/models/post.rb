8bf75ca7121321d20ace4fba3e79495ebc4470ae
Suhani Zaman <suhanizaman15@gmail.com>
2021-09-23 12:20:30 +0400

    perfectly working  delete button

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   has_one_attached :post_image
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :post_image, content_type: ['image/png', 'image/jpg', 'image/jpeg'],
             size: { less_than: 5.megabytes , message: 'Uploaded image size is too big' }
 end
