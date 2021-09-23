8f00db636a68dd3d23aa6877abd6ef686f56d564
Yousuf Mohamed <yousuf_mohamed@hotmail.com>
2021-09-23 17:11:52 +0100

    added tests for posting image

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -18,15 +18,27 @@ RSpec.describe PostsController, type: :controller do
   # end
 
   describe "user is not signed in" do
-
     it 'alerts please sign in' do
       session[:user_id] = nil
       get :index
       expect(flash[:alert]).to match('Please signup or login to view this page')
     end
-
   end
 
+  describe "POST /" do
+    it "Creates a post" do
+      session[:user_id] = 1
+      post :create, 
+      params: { 
+        post: {
+          message: "Hello, world!", 
+          user_id: 1, 
+          post_image: fixture_file_upload('test_image.jpg', 'image/jpg')
+          }
+        }
+      expect(response).to redirect_to(posts_path)
+    end
+  end
 end
 
 
