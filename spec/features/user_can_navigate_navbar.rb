5ea3b2377c470fc6017b3a11b99e02a19495b6cd
Suhani Zaman <suhanizaman15@gmail.com>
2021-09-24 15:26:25 +0400

    Navbar FT: feed,login,signup,logout, add post

--- spec/features/user_can_navigate_navbar.rb
+++ spec/features/user_can_navigate_navbar.rb
@@ -2,10 +2,32 @@ require 'rails_helper'
 require 'helper_methods'
 
 RSpec.feature "Navbar", type: :feature do
-  scenario "user can use navbar" do
+  scenario "user can naviagte to new post" do
     add_new_sample_user
     login
     click_link "New Post"
     expect(page).to have_current_path(new_post_path)
+  end
+  scenario "user can naviagte to feed" do
+    add_new_sample_user
+    login
+    click_link "Feed"
+    expect(page).to have_current_path(posts_path)
+  end
+  scenario "user can naviagte to logout" do
+    add_new_sample_user
+    login
+    click_link "Log Out"
+    expect(page).to have_current_path(login_path)
+  end
+  scenario "user can naviagte to logout" do
+    visit '/'
+    click_link "Log In"
+    expect(page).to have_current_path(login_path)
+  end
+  scenario "user can naviagte to signup" do
+    visit '/'
+    click_link "Sign Up"
+    expect(page).to have_current_path(signup_path)
   end
 end
