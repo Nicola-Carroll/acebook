f3db278090a4482300d2865743b61f26f6626ee8
Fiona Apps <wikipanyd@gmail.com>
2021-09-24 12:11:32 +0100

    passed basic search feature test

--- spec/features/user_can_search.rb
+++ spec/features/user_can_search.rb
@@ -8,10 +8,12 @@ RSpec.feature "Search", type: :feature do
         visit login_path
         fill_in :password, with: 'griltheAnim4lz'
         fill_in :email, with: 'hilly@example.com'
-        click_button 'Login'
+        click_button 'Log In'
     end
 
     scenario "Can use the search bar" do
-
+        fill_in "Search", with: "Boop"
+        click_button(class: 'search-btn')
+        expect(page).to have_current_path('/search?q=Boop')
     end
 end
