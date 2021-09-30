d4287cbab66a98742f510e25df8020b03f86189c
Nicola-Carroll <nicolaccarroll@gmail.com>
2021-09-30 14:48:08 +0100

    try again with emojify

--- app/helpers/emoji_helper.rb
+++ app/helpers/emoji_helper.rb
@@ -1,13 +1,16 @@
 module EmojiHelper
   def emojify(content)
-    (content).to_str.gsub(/:([\w+-]+):/) do |match|
-      if emoji = Emoji.find_by_alias($1).raw
-        %(<img alt="#$1" src="../public/images/emoji/unicode/#{emoji}" 
-        style="vertical-align:middle" width="20" height="20" />)
-      else
-        match
+    (content)
+      .to_str
+      .gsub(/:([\w+-]+):/) do |match|
+        if Emoji.find_by_alias($1) && emoji = Emoji.find_by_alias($1).raw
+          "<img alt=\"#{$1}\" src=\"../public/images/emoji/unicode/#{emoji}\" 
+        style=\"vertical-align:middle\" width=\"20\" height=\"20\" />"
+        else
+          match
+        end
+        p emoji
       end
-      p emoji 
-    end.html_safe if content.present?
+      .html_safe if content.present?
   end
-end 
+end
