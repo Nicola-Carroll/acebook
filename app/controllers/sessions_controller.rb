ef4c897159ce39e592abbc5844a17c2f00ae3f54
Nicola-Carroll <nicolaccarroll@gmail.com>
2021-09-30 13:54:23 +0100

    alter login redirect path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,16 @@
 class SessionsController < ApplicationController
   skip_before_action :authorised
 
-  def new
-  end
+  def new; end
 
   def create
     @user = User.find_by(email: params[:email])
 
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect_to posts_url
+      redirect_to '/posts'
     else
-      flash[:invalid] = "Invalid Email or Password"   
+      flash[:invalid] = 'Invalid Email or Password'
       redirect_to login_path
     end
   end
@@ -20,7 +19,6 @@ class SessionsController < ApplicationController
     flash.clear
     session[:user_id] = nil
     redirect_to login_path
-    flash[:loggedout] = "Successfully Logged Out"
+    flash[:loggedout] = 'Successfully Logged Out'
   end
-
 end
